25eccdcff6a9adaac084c549eaa2273bbc78f903
Win-yk <108043857+Win-yk@users.noreply.github.com>
2023-01-12 18:24:04 +0800

    title & date

--- bin/loadblog.py
+++ bin/loadblog.py
@@ -1,4 +1,4 @@
-import os,json
+import os, json, time
 
 with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "../blog/index.json")), "r", encoding = "utf-8") as f:
     bloginfo = json.load(f)
@@ -14,13 +14,14 @@ while True:
                 mxpid = max(mxpid, int(pid) + 1)
             else : mxpid += 1
             abstract = ""
-            with open(os.path.abspath(os.path.join(os.path.dirname(__file__), ".." + arr[2])), "r", encoding = "utf-8") as f:
+            blogpath = os.path.abspath(os.path.join(os.path.dirname(__file__), ".." + arr[2]))
+            with open(blogpath, "r", encoding = "utf-8") as f:
                 abstract = f.read(100)
-            bloginfo[pid] = {"title" : arr[1], "url" : arr[2], "abstract" : abstract}
+            bloginfo[pid] = {"title" : arr[1], "url" : arr[2], "abstract" : abstract, "mtime" : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.stat(blogpath).st_mtime))}
         elif arr[0] == 'del':
             del bloginfo[arr[1]]
     except (EOFError, KeyboardInterrupt):
         break
     
 with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "../blog/index.json")), "w", encoding = "utf-8") as f:
-    json.dump(bloginfo, f, ensure_ascii = False, sort_keys = True)
+    json.dump(bloginfo, f, ensure_ascii = False, sort_keys = True, indent = 4)
