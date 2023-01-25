c81fd9b2df69cf164d60328f3c78c13cd94b320a
Win-yk <108043857+Win-yk@users.noreply.github.com>
2023-01-25 13:24:18 +0800

    little optimize

--- js/flappybird.js
+++ js/flappybird.js
@@ -1,38 +1,44 @@
-var movingpipe1, movingpipe2, movingbird;
+var movingpipe1, movingpipe2, movingpipe3, movingbird;
+var birdpos = 200;
 var gamerunning = false, gamescore = 0, bestscore = 0;
-var clicked = false;
+var clicked = false, unabled = false;
 function setpipebox(pipeboxid){
     let ret = setInterval(function(){
-        let pipebox = document.getElementById(pipeboxid);
-        pipebox.style.display = "none";
-        pipebox.style.animation = "";
-        setTimeout(function(){
-            if(gamerunning){
-                let pos = Math.floor(Math.random() * 195);
-                pipebox.style.display = "block";
-                pipebox.style.top = (pos - 281) + "px";
-                pipebox.style.animation = "pipemoving 2s linear forwards";
-                setTimeout(function(){
-                    let R1 = pos + 40, L2 = R1 + 125;
-                    let p = Number(document.getElementById("birdbox").style.top.match(/\d+/)[0]) + 12.5;
-                    if(gamerunning && (p <= R1 || L2 <= p)) gameover();
-                    if(gamerunning) ++gamescore; else clearInterval(ret);
-                }, 1000);
-            }
-            else clearInterval(ret);
-        }, 5);
-    }, 2000);
+        if(gamerunning){
+            let pipebox = document.getElementById(pipeboxid);
+            pipebox.style = "";
+            setTimeout(function(){
+                if(gamerunning){
+                    let pos = Math.floor(Math.random() * 195);
+                    pipebox.style.top = (pos - 281) + "px";
+                    pipebox.style.animation = "pipemoving 2s linear forwards";
+                    setTimeout(function(){
+                        let R1 = pos + 40, L2 = R1 + 125, p = birdpos + 12.5;
+                        if(gamerunning && (p <= R1 || L2 <= p)) gameover();
+                        if(gamerunning) ++gamescore; else clearInterval(ret);
+                    }, 1000);
+                }
+                else clearInterval(ret);
+            }, 1000);
+        }
+    }, 3000);
     return ret;
 }
 function startpipemoving(){
     movingpipe1 = setpipebox("pipebox1");
     setTimeout(function(){
-        if(gamerunning) movingpipe2 = setpipebox("pipebox2");
+        if(gamerunning){
+            movingpipe2 = setpipebox("pipebox2");
+            setTimeout(function(){
+                if(gamerunning) movingpipe3 = setpipebox("pipebox3");
+            }, 1000);
+        }
     }, 1000);
 }
 function stoppipemoving(){
     clearInterval(movingpipe1); movingpipe1 = undefined;
     clearInterval(movingpipe2); movingpipe2 = undefined;
+    clearInterval(movingpipe3); movingpipe3 = undefined;
     let pipeboxes = document.getElementsByClassName("pipebox");
     for(let i = 0; i < pipeboxes.length; i++)
         pipeboxes[i].style.animationPlayState = "paused";
@@ -42,8 +48,7 @@ function startbirdmoving(){
     movingbird = setInterval(function(){
         if(gamerunning){
             let birdbox = document.getElementById("birdbox");
-            birdbox.style.top = 
-                Math.max(0, Math.min(400 - 1, Number(birdbox.style.top.match(/\d+/)[0]) + (clicked ? -30: 30))) + "px";
+            birdbox.style.top = (birdpos = Math.max(0, Math.min(400 - 1, birdpos + (clicked ? -30: 30)))) + "px";
             birdbox.style.transform = clicked ? "rotate(-30deg)": "rotate(30deg)";
             clicked = false;
         }
@@ -53,20 +58,23 @@ function stopbirdmoving(){
     clearInterval(movingbird); movingbird = undefined;
 }
 function gameover(){
+    unabled = true;
     stopbirdmoving();
     stoppipemoving();
     gamerunning = false;
     document.getElementById("score").innerHTML = gamescore;
     document.getElementById("bestscore").innerHTML = (bestscore = Math.max(gamescore, bestscore));
     document.getElementById("board").style.display = "block";
+    setTimeout(function(){unabled = false;}, 5000);
 }
 window.onload = function(){
     document.getElementById("flappybird").addEventListener("click", function(){
+        if(unabled) return;
         if(!gamerunning){
             let nodes = this.childNodes;
             for(let i = 0; i < nodes.length; i++)
                 nodes[i].style = "";
-            document.getElementById("birdbox").style.top = "200px";
+            birdpos = 200;
             gamerunning = true; gamescore = 0; clicked = false;
             startpipemoving();
             startbirdmoving();
